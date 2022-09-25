95cf64cc7305012ece6381f646df7d6abe7aa800
aixeria <108988246+aixeria@users.noreply.github.com>
2022-09-25 02:58:30 +0100

    add vulnus metadata

--- src/vulnus.rs
+++ src/vulnus.rs
@@ -13,8 +13,67 @@ pub struct Map {
     #[serde(rename = "_name")] 
     pub(crate) name: String,
     #[serde(rename = "_notes")] 
-    pub(crate) notes : Vec<MapNote>
+    pub(crate) notes : Vec<MapNote>,
+    #[serde(skip)]
+    pub(crate) meta_data: Option<MetaData>
+
 }
+#[derive(Debug, Deserialize,Serialize)]
+pub struct MetaData {
+    #[serde(rename = "_artist")] 
+    pub(crate) artist: String,
+    #[serde(rename = "_difficulties")] 
+    pub(crate) difficulties: Vec<String>,
+    #[serde(rename = "_mappers")] 
+    pub(crate) name: Vec<String>,
+    #[serde(rename = "_music")] 
+    pub(crate) music : String,
+    #[serde(rename = "_title")] 
+    pub(crate) title : String,
+    #[serde(rename = "_version")] 
+    pub(crate) version : u32,
+}
+
+impl MetaData {
+    pub fn set_artist(&mut self, artist: String) {
+        self.artist = artist;
+    }
+    pub fn set_title(&mut self, title: String) {
+        self.title = title;
+    }
+    pub fn set_music(&mut self, music: String) {
+        self.music = music;
+    }
+    pub fn set_version(&mut self, version: u32) {
+        self.version = version;
+    }
+    pub fn set_difficulties(&mut self, difficulties: Vec<String>) {
+        self.difficulties = difficulties;
+    }
+    pub fn set_mappers(&mut self, mappers: Vec<String>) {
+        self.name = mappers;
+    }
+    pub fn get_artist(&self) -> &String {
+        &self.artist
+    }
+    pub fn get_title(&self) -> &String {
+        &self.title
+    }
+    pub fn get_music(&self) -> &String {
+        &self.music
+    }
+    pub fn get_version(&self) -> &u32 {
+        &self.version
+    }
+    pub fn get_difficulties(&self) -> &Vec<String> {
+        &self.difficulties
+    }
+    pub fn get_mappers(&self) -> &Vec<String> {
+        &self.name
+    }
+
+}
+
 
 impl Map {
     pub fn set_offset(&mut self, offset: Duration) {
@@ -45,6 +104,12 @@ impl Map {
     pub fn get_at(&self) -> f32 {
         self.approach_time
     }
+
+    // meta data
+    pub fn add_metadata(&mut self, meta_data: MetaData) {
+        self.meta_data = Some(meta_data);
+    }
+
 }
 impl From<soundspace::Map> for Map {
     fn from(m: soundspace::Map) -> Self {
@@ -52,7 +117,8 @@ impl From<soundspace::Map> for Map {
             approach_distance: 0.0,
             approach_time: 0.0,
             name: String::from(""),
-            notes: m.notes.into_iter().map(|n| MapNote::from(n)).collect()
+            notes: m.notes.into_iter().map(|n| MapNote::from(n)).collect(),
+            meta_data: None
         }
     }
 }
